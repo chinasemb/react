50f1342b9ae873d654c0576b60d307aaabebaed2
chinasemb <1032944759@qq.com>
2019-09-12 11:06:33 +0800

    layoutstyledcomponents

--- simple_book02/src/common/header/style.js
+++ simple_book02/src/common/header/style.js
@@ -24,5 +24,68 @@ export const Nav = styled.div `
     width: 960px;
     height: 100%;
     margin: 0 auto;
-    background: #00ff00;
+    padding-right: 70px;
+    box-sizing: border-box;
+`
+
+export const NavItem = styled.div `
+    line-height: 56px;
+    padding: 0 15px;
+    fontsize: 17px;
+    &.left {
+        float: left;
+    }
+    &.right {
+        float: right;
+        color: #969696;
+    }
+    &.active {
+        color: #ea6f5a
+    }
+`
+// 有了padding width 就会从160px 撑开为 200px,不像撑开
+// 就要设置 box-sizing: border-box;
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+}) `
+    width: 160px;
+    height: 38px;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    margin-top: 9px;
+    margin-left: 20px;
+    padding: 0 20px;
+    box-sizing: border-box;
+    font-size: 14px;
+    &::placeholder {
+        color: #808080;
+    }
+`
+
+export const Addition = styled.div `
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+
+`
+
+export const Button = styled.div `
+    float: right;
+    line-height: 38px;
+    margin-top: 9px;
+    margin-right: 20px;
+    padding: 0 20px;
+    border-radius: 19px;
+    border: 1px solid #ec6149;
+    font-size: 14px;
+    &.reg {
+        color: #ec6149;
+    }
+    &.writing {
+        color: #fff;
+        background: #ec6149;
+    }
 `
