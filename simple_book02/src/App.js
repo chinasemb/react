aea426d9b6a9d5a73f04d81585f16391fa607855
chinasemb <1032944759@qq.com>
2019-09-12 08:56:48 +0800

    7.1

---
+++ simple_book02/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GlobalStyle } from './style';
+
+
+function App() {
+  return (
+    <div className="color1">
+        Hello World
+        <GlobalStyle blueColor/>
+    </div>
+  );
+}
+
+export default App;
