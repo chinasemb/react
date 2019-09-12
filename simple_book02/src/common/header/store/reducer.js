9b1f9356392da988b17d88d3f60cd858df104caf
chinasemb <1032944759@qq.com>
2019-09-12 19:35:57 +0800

    immutable

--- simple_book02/src/common/header/store/reducer.js
+++ simple_book02/src/common/header/store/reducer.js
@@ -1,19 +1,17 @@
 import * as constants from './constants'
+import { fromJS } from 'immutable'
 
-const defaultState = {
+const defaultState = fromJS({
     focused: false
-}
+})
 
 export default (state = defaultState, action) => {
     if( action.type === constants.SEARCH_FOCUS ) {
-        return {
-            focused: true
-        }
+        // immutable 对象的set 方法会结合之前immutable 对象的值和设置的值，返回一个全新的对象，并不会该原来的数据
+        return state.set('focused', true)
     }
     if( action.type === constants.SEARCH_BLUR ) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
     }
     return state
 }
