650bd8c6d73271525e730dce7ddccd8d7ffd76c8
chinasemb <1032944759@qq.com>
2019-09-12 22:37:18 +0800

    ajax

--- simple_book02/src/common/header/index.js
+++ simple_book02/src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { actionCreators } from './store'
 import { CSSTransition } from 'react-transition-group'
@@ -18,83 +18,87 @@ import {
     SearchInfoList
  } from './style'
 
-const getListArea = (show) => {
-    if (show) {
+class Header extends Component {
+    getListArea() {
+        if (this.props.focused) {
+            return (
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch>
+                            {/* <i className='iconfont'>&#xe600;</i> */}
+                            换一批
+                        </SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        {
+                            this.props.list.map((item)=> {
+                                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+                            })
+                        }
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+        }else {
+            return null
+        }
+    }
+
+    render() {
+        const { focused, handleInputFocus, handleInputBlur } = this.props
         return (
-            <SearchInfo>
-                <SearchInfoTitle>
-                    热门搜索
-                    <SearchInfoSwitch>
-                        {/* <i className='iconfont'>&#xe600;</i> */}
-                        换一批
-                    </SearchInfoSwitch>
-                </SearchInfoTitle>
-                <SearchInfoList>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                    <SearchInfoItem>简书</SearchInfoItem>
-                </SearchInfoList>
-            </SearchInfo>
+            <HeaderWrapper>
+                <Logo></Logo>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>
+                        <i className='iconfont'>&#xe636;</i>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            timeout={200}
+                            in={focused}
+                            classNames='slide'
+                        >
+                            <NavSearch
+                                className= { focused ? 'focused' : '' }
+                                onFocus= {handleInputFocus}
+                                onBlur= {handleInputBlur}
+                            >
+                            </NavSearch>
+                        </CSSTransition>
+                        <i className= { focused ? 'focused iconfont' : 'iconfont' }>&#xe615;</i>
+                        { this.getListArea(focused) }
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writing'>
+                        <i className='iconfont'>&#xe600;</i>
+                        写文章
+                    </Button>
+                    <Button className='reg'>注册</Button>
+                </Addition>
+            </HeaderWrapper>
         )
-    }else {
-        return null
     }
 }
 
 // 无状态组件，性能比较高
-const Header = (props) => {
-    const { focused, handleInputFocus, handleInputBlur } = props
-    return (
-        <HeaderWrapper>
-            <Logo></Logo>
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载App</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'>
-                    <i className='iconfont'>&#xe636;</i>
-                </NavItem>
-                <SearchWrapper>
-                    <CSSTransition
-                        timeout={200}
-                        in={focused}
-                        classNames='slide'
-                    >
-                        <NavSearch
-                            className= { focused ? 'focused' : '' }
-                            onFocus= {handleInputFocus}
-                            onBlur= {handleInputBlur}
-                        >
-                        </NavSearch>
-                    </CSSTransition>
-                    <i className= { focused ? 'focused iconfont' : 'iconfont' }>&#xe615;</i>
-                    { getListArea(focused) }
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className='writing'>
-                    <i className='iconfont'>&#xe600;</i>
-                    写文章
-                </Button>
-                <Button className='reg'>注册</Button>
-            </Addition>
-        </HeaderWrapper>
-    )
-}
+// const Header = (props) => {
+    
+// }
 
 const mapState = (state) => ({
-    focused: state.getIn(['header','focused'])
+    focused: state.getIn(['header','focused']),
+    list: state.getIn(['header','list'])
 })
 
 const mapDispatch = (dispatch) => ({
     handleInputFocus() {
         dispatch(actionCreators.searchFocus())
+        dispatch(actionCreators.getList())
     },
     handleInputBlur() {
         dispatch(actionCreators.searchBlur())
